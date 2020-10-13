6cd1b4a8f7722d1f4dcba126cdf8878d56e5ff21
javier <javier@dojodevs.io>
2020-10-13 07:53:52 -0500

    shop page, collection preview component and collection item component. Just styles, non functionality yet

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
-import { Switch, Route, Link } from 'react-router-dom';
 
 import './homepage.styles.scss';
 
 const HomePage = () => {
   return (
     <div className='homepage'>
-      <Switch>
-        <Route exact path='/' component={Directory}/>
-        <Route exact path='/hats' component={<div>Hello</div>}/>
-      </Switch>
       <Directory />
     </div>
   )
