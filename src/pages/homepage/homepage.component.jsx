bb87a68d83f8181f5d9606e684c43f852fa08023
javier <javier@dojodevs.io>
2020-10-13 01:44:14 -0500

    Basic navigation setup

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
-import MenuItem from '../../components/menu-item/menu-item.component';
+import { Switch, Route, Link } from 'react-router-dom';
+
 import './homepage.styles.scss';
 
 const HomePage = () => {
   return (
     <div className='homepage'>
+      <Switch>
+        <Route exact path='/' component={Directory}/>
+        <Route exact path='/hats' component={<div>Hello</div>}/>
+      </Switch>
       <Directory />
     </div>
   )
