1b7f69e2ae35a2435b1147b70f7f16692bb57749
javier <javier@dojodevs.io>
2020-10-16 02:17:34 -0500

    one UserActionTypes added and signin restriction when user is already signedin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -44,16 +44,20 @@ class App extends Component {
         <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
-          <Route exact path='/signin' component={Authentication}/>
           <Route path='/shop' component={ShopPage}/>
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<Authentication />)}/>
         </Switch>
       </div>
     );
   };
 };
 
-const mapsDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
 
-export default connect(null, mapsDispatchToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
