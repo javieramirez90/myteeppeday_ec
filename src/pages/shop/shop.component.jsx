d6a4bed6f44321f737a96e47c0a09aa5c741a9c8
javier <javier@dojodevs.io>
2020-10-19 11:51:40 -0500

    shop and directory own reducers and selectors. collection-overview own component. About to start with nested routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,13 @@
-import React, { Component } from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { SHOP_DATA } from './shop.data';
+import React from 'react';
 
-export default class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  };
-
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
-      </div>
-    );
-  };
+const ShopPage = () => {
+  return (
+    <div className='shop-page'>
+      <CollectionOverview />
+    </div>
+  );
 };
+
+export default ShopPage;
