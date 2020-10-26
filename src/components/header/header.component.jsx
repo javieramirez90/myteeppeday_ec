a67f9e2be864d29e1b1002c5430473b620e9d59a
javier <javier@dojodevs.io>
2020-10-25 23:09:33 -0600

    'as' styled-component property added to header component.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { ReactComponent as Logo } from '../../assets/dojo.svg';
 
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 
 const Header = ({ currentUser, cartHidden }) => {
   console.log(currentUser)
@@ -23,7 +23,7 @@ const Header = ({ currentUser, cartHidden }) => {
         <OptionLink to='/contact'>CONTACT</OptionLink>
         {
           currentUser ?
-          <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+          <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
           :
           <OptionLink to='/signin'>SIGN IN</OptionLink>
         }
